93fa8227dc9bcd39da878aae643487bc5df9b697
shinoda <tetora1053@gmail.com>
2019-07-04 01:58:34 +0900

    add convert functions comment ammended

--- src/temperature_calculator.js
+++ src/temperature_calculator.js
@@ -49,3 +49,21 @@ class Calculator extends React.Component {
         );
     }
 }
+
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+    return (celcius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+        return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+}
