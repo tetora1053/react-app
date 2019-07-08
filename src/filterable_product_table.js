f1d89e5cfd720137241accb007dd9f0c7021c2a8
shinoda <tetora1053@gmail.com>
2019-07-08 23:04:12 +0900

    implement filtering by stocked prop.

--- src/filterable_product_table.js
+++ src/filterable_product_table.js
@@ -1,26 +1,54 @@
 import React from 'react';
 
-export default function FilterableProductTable(props) {
-    console.log(props.products);
-    return (
-        <div>
-            <SearchBar />
-            <ProductTable products={props.products}/>
-        </div>
-    );
+export default class FilterableProductTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isShowProductsInStock: false,
+        }
+        this.SwitchShowProductsInStock = this.SwitchShowProductsInStock.bind(this);
+    }
+
+    SwitchShowProductsInStock(e) {
+        console.log('checkbox value : ', e.target.checked);
+        this.setState({isShowProductsInStock: e.target.checked});
+    }
+
+    render() {
+        console.log(this.props.products);
+        let products = this.props.products;
+        if (this.state.isShowProductsInStock) {
+            console.log(111);
+            products = products.filter((product) => {return product.stocked;});
+        }
+        return (
+            <div>
+                <SearchBar switchShowProductsInStock={this.SwitchShowProductsInStock} />
+                <ProductTable products={products}/>
+            </div>
+        );
+    }
 }
 
-function SearchBar() {
-    return (
-        <form>
-            <input type="text" placeholder="Search..." />
-            <p>
-                <input type="checkbox" />
-                {' '}
-                Only show products in stock
-            </p>
-      </form>
-    );
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render () {
+        return (
+            <form>
+                <input type="text" placeholder="Search..." />
+                <p>
+                    <label>
+                        <input type="checkbox" onChange={this.props.switchShowProductsInStock}/>
+                        {' '}
+                        Only show products in stock
+                    </label>
+                </p>
+            </form>
+        );
+    }
 }
 
 function ProductTable(props) {
